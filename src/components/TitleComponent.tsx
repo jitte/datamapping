4e8aeda1cebf651f4286568f84c19a8a345e5d3d
jitte <jitte@jitte.net>
2023-05-05 10:30:40 +0900

    changed style

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -1,4 +1,5 @@
 import Icon from '../Icon';
+import { TrashIcon } from '@heroicons/react/24/outline';
 
 export default function TitleComponent(
   { name, description }: { name: string; description: string; }
@@ -9,9 +10,9 @@ export default function TitleComponent(
       <div className="flex flexitems-center justify-between bg-gray-50 rounded-t-lg p-2">
         <Icon name={iconName} />
         <div className="text-lg">{name}</div>
-        <Icon name="delete_button" style={{ width: '18px', height: '18px'}} />
+        <TrashIcon className="h-4 w-4" style={{color: '#777'}} />
       </div>
-      <div className="text-gray-500 px-5 pb-3 text-xs">
+      <div className="text-gray-500 px-5 py-2 text-xs">
         {description}
       </div>
     </>
