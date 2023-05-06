fca8fd7c610111b1b9f746d9c669e419013c347b
jitte <jitte@jitte.net>
2023-05-06 14:58:49 +0900

    changed reference key for Icon

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -4,21 +4,19 @@ import {
   CpuChipIcon,
   DocumentDuplicateIcon,
   Bars2Icon,
-  TrashIcon,
 } from '@heroicons/react/24/outline';
 
-const icon_name :any = {
-    'pii_subject': UsersIcon,
-    'pii_controller': CogIcon,
-    'pii_processor': CpuChipIcon,
-    'third_party': DocumentDuplicateIcon,
-    'drag_handle': Bars2Icon,
-    'delete_button': TrashIcon,
-};
-
 export default function Icon({name, style={ color: '#777'}}: any): JSX.Element {
-    const IconName = icon_name[name];
-    return (
-      <IconName className="h-6 w-6" style={style} />
-    );
-};
+  const iconTypes: {[key: string]: any} = {
+      piiSubject: UsersIcon,
+      piiController: CogIcon,
+      piiProcessor: CpuChipIcon,
+      thirdParty: DocumentDuplicateIcon,
+      dragHandle: Bars2Icon,
+  };
+
+  const IconComponent = iconTypes[name];
+  return (
+    <IconComponent className="h-6 w-6" style={style} />
+  );
+}
