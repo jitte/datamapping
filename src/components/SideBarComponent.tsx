0ea694b68a964577b50b7916b8631dcd8c36ad85
jitte <jitte@jitte.net>
2023-05-06 15:06:07 +0900

    drag and drop

--- src/components/SideBarComponent.tsx
+++ src/components/SideBarComponent.tsx
@@ -1,24 +1,34 @@
+import { nodeTitles } from '../CustomNodes';
 import Icon from '../Icon'
 
+function EntityComponent({nodeType}: {nodeType: string}) : JSX.Element {
+  const onDragStart = (event: any, type: any) => {
+    event.dataTransfer.setData('application/reactflow', type);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <div
+      className="flex border border-solid border-black rounded-lg border-l-4 p-1 m-1 items-center gap-2"
+      onDragStart={(event) => onDragStart(event, nodeType)}
+      draggable
+    >
+      <Icon name={nodeType} />
+      <div className="text-sm">{nodeTitles[nodeType]}</div>
+      <div className="flex-grow" />
+      <Icon name="dragHandle" />
+    </div>
+  );
+}
+
 export default function SideBarComponent() {
-  function EntityItem({text} : any) : JSX.Element {
-    const name = text.toLowerCase().replace(/ /g, '_');
-    return (
-      <div className="flex border border-solid border-black rounded-lg border-l-4 p-1 m-1 items-center gap-2" id={name}>
-        <Icon name={name} />
-        <div className="text-sm">{text}</div>
-        <div className="flex-grow" />
-        <Icon name="drag_handle" />
-      </div>
-    );
-  }
   return (
     <nav className="w-52 h-full flex flex-col border-r" >
       <span className="text-center">Entities</span>
-      <EntityItem text="PII Subject" />
-      <EntityItem text="PII Controller" />
-      <EntityItem text="PII Processor" />
-      <EntityItem text="Third Party" />
+      <EntityComponent nodeType="piiSubject" />
+      <EntityComponent nodeType="piiController" />
+      <EntityComponent nodeType="piiProcessor" />
+      <EntityComponent nodeType="thirdParty" />
     </nav>
   );
-};
+}
