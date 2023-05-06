a75444ec3f7c98d2a44fd6a197dbf4cf8b0bac04
jitte <jitte@jitte.net>
2023-05-06 15:00:19 +0900

    changed reference for Icon

--- src/components/TitleComponent.tsx
+++ src/components/TitleComponent.tsx
@@ -1,20 +1,21 @@
 import { useContext } from 'react';
 import { TrashIcon } from '@heroicons/react/24/outline';
 import { GlobalContext } from '../Contexts';
+import { nodeTitles } from '../CustomNodes';
+
 import Icon from '../Icon';
 
 export default function TitleComponent(
-  { name, description, nodeId } : { name: string, description: string, nodeId: string }
+  { nodeType, description, nodeId } : { nodeType: string, description: string, nodeId: string }
 ) : JSX.Element {
-  const iconName = name.toLowerCase().replace(/ /g, '_');
 
   // グローバルコンテキストで作成した削除関数を使う
   const { deleteNode } = useContext(GlobalContext);
   return (
     <>
       <div className="flex flexitems-center justify-between bg-gray-50 rounded-t-lg p-2">
-        <Icon name={iconName} />
-        <div className="text-lg">{name}</div>
+        <Icon name={nodeType} />
+        <div className="text-lg">{nodeTitles[nodeType]}</div>
         <button onClick={() => deleteNode(nodeId)}>
           <TrashIcon className="h-4 w-4" style={{color: '#777'}} />
         </button>
