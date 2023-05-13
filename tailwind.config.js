b34d705ffdaea66b6a41f631028afabd8b5c9c64
jitte <jitte@jitte.net>
2023-05-13 14:02:41 +0900

    added flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,tsx,jsx}"],
+  content: [
+    './src/**/*.{js,ts,tsx,jsx}',
+    'node_modules/flowbite-react/**/*.{js.jsx,ts,tsx}'
+  ],
   darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
