393d552fe80f3d119d7941cd038b02fe9366675a
jitte <jitte@jitte.net>
2023-05-21 21:26:25 +0900

    removed flowbite

--- src/ProjectsView/index.tsx
+++ src/ProjectsView/index.tsx
@@ -1,61 +1,64 @@
 import {
-	TextInput,
-	Button,
-	Table,
-} from 'flowbite-react';
-import {
 	PlusIcon,
-	MagnifyingGlassIcon
 } from '@heroicons/react/24/outline';
 
+const initialProjects = [
+	{
+		id: 1,
+		name: "Project 1",
+		subjects: ["subject1", "subject2"],
+		controllers: ["controller1"],
+		processors: ["processor1", "processor2"]
+	},
+	{
+		id: 2,
+		name: "Project 2",
+		subjects: ["subject1", "subject2"],
+		controllers: ["controller1"],
+		processors: ["processor1", "processor2"]
+	},
+	{
+		id: 3,
+		name: "Project 3",
+		subjects: ["subject1", "subject2"],
+		controllers: ["controller1"],
+		processors: ["processor1", "processor2"]
+	},
+]
+
 export default function ProjectsView() {
+	const data = initialProjects
   return (
-		<div className="flex flex-col m-2">
-			<div className="flex flex-row items-center">
-				<TextInput
-					className="grow m-2"
-					icon={MagnifyingGlassIcon}
-					placeholder='Search text'
-				/>
-				<Button className="bg-blue-500 m-2">
+		<div className="flex flex-col m-4">
+			<div className="flex flex-row items-center m-2">
+				<button className="text-white bg-blue-700 flex flex-row p-2 rounded-lg">
 					<PlusIcon className="mr-2 h-5 w-5" />
 					Add Project
-				</Button>
+				</button>
 			</div>
-			<Table className="m-2">
-				<Table.Head>
-					<Table.HeadCell>
-						ID
-					</Table.HeadCell>
-					<Table.HeadCell>
-						Division
-					</Table.HeadCell>
-					<Table.HeadCell>
-						Project Name
-					</Table.HeadCell>
-					<Table.HeadCell>
-						Category
-					</Table.HeadCell>
-					<Table.HeadCell>
-					</Table.HeadCell>
-				</Table.Head>
-				<Table.Body>
-					<Table.Row>
-						<Table.Cell>
-							1
-						</Table.Cell>
-						<Table.Cell>
-							HQ
-						</Table.Cell>
-						<Table.Cell>
-							Data Service
-						</Table.Cell>
-						<Table.Cell>
-							Cloud
-						</Table.Cell>
-					</Table.Row>
-				</Table.Body>
-			</Table>
+			<table className="text-left text-sm drop-shadow-md m-2">
+				<thead>
+					<tr className="bg-gray-50 uppercase min-w-0">
+						<th className="px-6 py-4">ID</th>
+						<th className="px-6 py-4">Name</th>
+						<th className="px-6 py-4">Subject</th>
+						<th className="px-6 py-4">Controller</th>
+						<th className="px-6 py-4">Processor</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((pj,idx) => (
+						<tr key={idx} className="border-b min-w-0">
+							<td className="px-6 py-4">{pj.id}</td>
+							<td className="px-6 py-4">{pj.name}</td>
+							<td className="px-6 py-4">{pj.subjects.join(", ")}</td>
+							<td className="px-6 py-4">{pj.controllers.join(", ")}</td>
+							<td className="px-6 py-4">{pj.processors.join(", ")}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			
 		</div>
   );
 }
