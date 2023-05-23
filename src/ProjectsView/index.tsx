73476c18bdf1f96a992a1f468d0e894ad2d4be68
jitte <jitte@jitte.net>
2023-05-23 21:45:35 +0900

    edit project

--- src/ProjectsView/index.tsx
+++ src/ProjectsView/index.tsx
@@ -1,57 +1,60 @@
 import { useContext, FormEvent } from 'react'
-import { Dialog } from "@headlessui/react";
+import { Dialog } from '@headlessui/react'
 import {
 	PlusIcon,
 	PencilIcon,
 	TrashIcon,
 	IdentificationIcon,
-} from '@heroicons/react/24/outline';
+} from '@heroicons/react/24/outline'
 
 import { PopUpContext } from '../Contexts'
-import { useLocalStore } from '../store';
+import { useLocalStore } from '../store'
 
 export default function ProjectsView() {
 	const projects  = useLocalStore((state) => state.projects)
 	const setProjects = useLocalStore((state) => state.setProjects)
 	const newProjectId = useLocalStore((state) => state.newProjectId)
 
-	const {openPopUp, closePopUp} = useContext(PopUpContext)
+	const { openPopUp, closePopUp } = useContext(PopUpContext)
 
-	function handleFlow(id:any) {
+	function handleFlow(id: any) {
 
 	}
+
 	function handleSubmit(event: FormEvent, id: number) {
-		event.preventDefault();
-		const { value: name } = (event.target as any).name;
-		const { value: description } = (event.target as any).description;
+		event.preventDefault()
+		// collect form values
+		const { value: name } = (event.target as any).name
+		const { value: description } = (event.target as any).description
+		// project to be added
 		const newProject = { id, name, description }
-		setProjects([...projects, newProject])
-		console.log({ at: "handleSubmit", name, description });
-		closePopUp();
+		console.log({ at: 'handleSubmit', newProject })
+		// update projects
+		setProjects([newProject, ...projects.filter((pj) => pj.id !== id)])
+		closePopUp()
 	}
-	function handleEdit(id:any) {
-		console.log({at: handleEdit, id})
+
+	function handleEdit(id: number | null) {
+		console.log({ at: 'handleEdit', id })
+		const project =  projects.find((pj) => pj.id === id) ?? { id: newProjectId(), name: '', description: '' }
+
 		openPopUp(
-			<Dialog
-				open={true}
-				onClose={closePopUp}
-				className="z-50"
-			>
+			<Dialog open={true} onClose={closePopUp} className="z-50" >
 				<div className="fixed inset-0 bg-black/30 flex items-center justify-center">
 					<Dialog.Panel className="bg-white rounded-xl">
 						<Dialog.Title className="bg-gray-100 rounded-t-xl p-4 drop-shadow text-center">
 							Project Dialog
 						</Dialog.Title>
-						<form onSubmit={(event) => handleSubmit(event, id)}>
+						<form onSubmit={(event) => handleSubmit(event, project.id)}>
 							<div className="flex flex-col p-4 text-sm">
 								<label htmlFor="name">
 									Project Name
 								</label>
-								<input type="text" name="name" className="form-input rounded" />
+								<input type="text" name="name" className="form-input rounded" defaultValue={project.name} />
 								<label htmlFor="description">
 									Description
 								</label>
-								<textarea name="description" className="form-textarea rounded" />
+								<textarea name="description" className="form-textarea rounded" defaultValue={project.description} />
 							</div>
 							<div className="flex flex-row justify-evenly pb-4">
 								<input type="submit" className="bg-blue-700 hover:bg-blue-800 text-white rounded-full px-4" value="Submit"/>
@@ -63,15 +66,17 @@ export default function ProjectsView() {
 			</Dialog>
 		)
 	}
+
 	function handleDelete(id:any) {
-		setProjects(projects.filter((pj)=>pj.id != id))
+		setProjects(projects.filter((pj)=>pj.id !== id))
 	}
+
   return (
 		<div className="flex flex-col p-2">
 			<div className="m-2">
 				<button
 					className="text-white bg-blue-700 hover:bg-blue-800 flex flex-row p-2 rounded-lg float-right"
-					onClick={() => handleEdit(newProjectId())}
+					onClick={() => handleEdit(null)}
 				>
 					<PlusIcon className="mr-2 h-5 w-5" />
 					Project
@@ -112,5 +117,5 @@ export default function ProjectsView() {
 				</tbody>
 			</table>
 		</div>
-  );
+  )
 }
