5e8b8ccfc5f710ba08125aa4bb36ca0de861fb99
jitte <jitte@jitte.net>
2023-05-28 14:00:35 +0900

    export LocalStoreType

--- src/store.tsx
+++ src/store.tsx
@@ -12,7 +12,7 @@ type ProjectType = {
   }
 }
 
-type LocalStoreType = {
+export type LocalStoreType = {
   projects: ProjectType[],
   setProjects: (projects: ProjectType[]) => void,
   newProjectId: () => number,
