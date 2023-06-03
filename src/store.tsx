1e147755b6c72efb9e607d7d9c61a0e0b1378f0b
jitte <jitte@jitte.net>
2023-06-03 19:05:22 +0900

    added newNodeId

--- src/store.tsx
+++ src/store.tsx
@@ -6,6 +6,7 @@ export type LocalStoreType = {
   projects: ProjectType[],
   setProjects: (projects: ProjectType[]) => void,
   newProjectId: () => number,
+  newNodeId: () => string,
 }
 
 export const useLocalStore = create(
@@ -17,6 +18,18 @@ export const useLocalStore = create(
       },
       newProjectId: () => {
         return 1 + Math.max(0, ...get().projects.map((project)=>project.id))
+      },
+      newNodeId: () => {
+        // ['node_1', 'node_2', ...]
+        const nodeIds = get().projects.map((project: ProjectType) => 
+          (project.nodes ?? []).map((node) => node.id)
+        ).reduce((acc, ele) => acc.concat(ele))
+        // [1, 2, ...]
+        const ids = nodeIds.map((nodeId: string) => {
+          const id = Number(nodeId.replace('node_', ''))
+          return Number.isNaN(id) ? 0 : id
+        })
+        return 'node_' + (1 + Math.max(0, ...ids))
       }
     }),
     {
