954975b5025f7b99f1a527f67bc7a27856a4f4a3
jitte <jitte@jitte.net>
2023-06-06 05:53:32 +0900

    moved ProjectList to TopRightPanel

--- src/components/Panels.tsx
+++ src/components/Panels.tsx
@@ -17,52 +17,48 @@ function ProjectList() {
   //console.log({ at: 'ProjectList', projects, currentProject })
   return (
     <Listbox value={currentProject} onChange={setCurrentProject}>
-      <div className="relative">
-        <Listbox.Button onClick={()=>setEntityMenuOpen(false)} className="
-          relative w-52 cursor-default rounded-lg bg-white py-2 pl-3 pr-3
-          text-left shadow-md focus:outline-none focus-visible:border-indigo-500
-          focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75
-          focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm
-        ">
-          <div className="flex flex-row gap-2">
-            <div className="grow">{currentProject.name}</div>
-            <ChevronUpDownIcon className="h-5 w-5" />
-          </div>
-        </Listbox.Button>
-        <Listbox.Options className="
-          absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1
-          text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm
-        ">
-          {projects.map((project) => (
-            <Listbox.Option
-              key={project.id}
-              className={({ active }) =>
-                `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                  active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                }`
-              }
-              value={project}
-            >
-              {({ selected }) => (
-                <>
-                  <span
-                    className={`block truncate ${
-                      selected ? 'font-medium' : 'font-normal'
-                    }`}
-                  >
-                    {project.name}
+      <Listbox.Button
+        onClick={()=>setEntityMenuOpen(false)}
+        className="w-52 text-left py-2 px-3"
+      >
+        <div className="flex flex-row gap-2">
+          <div className="grow">{currentProject.name}</div>
+          <ChevronUpDownIcon className="h-5 w-5" />
+        </div>
+      </Listbox.Button>
+      <Listbox.Options className="
+        absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1
+        text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm
+      ">
+        {projects.map((project) => (
+          <Listbox.Option
+            key={project.id}
+            className={({ active }) =>
+              `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+              }`
+            }
+            value={project}
+          >
+            {({ selected }) => (
+              <>
+                <span
+                  className={`block truncate ${
+                    selected ? 'font-medium' : 'font-normal'
+                  }`}
+                >
+                  {project.name}
+                </span>
+                {selected ? (
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
                   </span>
-                  {selected ? (
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                    </span>
-                  ) : null}
-                </>
-              )}
-            </Listbox.Option>
-          ))}
-        </Listbox.Options>
-      </div>
+                ) : null}
+              </>
+            )}
+          </Listbox.Option>
+        ))}
+      </Listbox.Options>
     </Listbox>
   )
 }
@@ -299,7 +295,6 @@ export function TopLeftPanel() {
   return (
     <Panel position='top-left'>
       <div className="flex flex-row items-center gap-2">
-        <ProjectList />
         <div className="rounded-md shadow-md text-sm font-medium bg-white inline-flex items-center">
           <EntityMenu />
         </div>
@@ -314,8 +309,18 @@ export function TopLeftPanel() {
 export function TopRightPanel() {
   return (
     <Panel position='top-right'>
-      <div className="rounded-md shadow-md text-sm font-medium bg-white inline-flex items-center">
-        <ConfigMenu />
+      <div className="flex flox-row">
+        <div className="
+          relative cursor-default rounded-lg bg-white
+          shadow-md focus:outline-none focus-visible:border-indigo-500
+          focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75
+          focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm
+        ">
+          <ProjectList />
+        </div>
+        <div className="rounded-md shadow-md text-sm font-medium bg-white inline-flex items-center">
+          <ConfigMenu />
+        </div>
       </div>
     </Panel>
   )
