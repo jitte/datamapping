32278a684ff24749a91c9e33cf28eb51103f70d3
jitte <jitte@jitte.net>
2023-06-06 21:52:21 +0900

    react-country-flag instead of emoji

--- src/components/ComboboxComponent.tsx
+++ src/components/ComboboxComponent.tsx
@@ -2,6 +2,7 @@ import { useState, Fragment } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { countries } from 'countries-list'
+import { ReactCountryFlag } from 'react-country-flag'
 
 const countryInfo:
 { [key: string] : { name: string, emoji: string } } = {
@@ -37,7 +38,11 @@ export default function ComboboxComponent( { name, caption, itemList, data }: Co
   }
   function flagAndCountry(countryCode: string) {
     const data = countryInfo[countryCode]
-    return data == undefined ? '' : `${data['emoji']} ${data['name']}`
+    return data == undefined ? '' : 
+      <div className="flex flex-row items-center gap-2">
+        <ReactCountryFlag svg countryCode={countryCode} className="border"/>
+        {data['name']}
+      </div>
   }
 
   return (
@@ -47,69 +52,74 @@ export default function ComboboxComponent( { name, caption, itemList, data }: Co
       </div>
       <div className="mt-2">
         <Combobox name={name} value={selectedItem} onChange={handleCombobox}>
-          <div className="relative">
-            <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow">
-              <Combobox.Input
-                className="nodrag w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 rounded-lg"
-                placeholder="Choose an option"
-                displayValue={() => flagAndCountry(selectedItem)}
-                onChange={(event) => setQuery(event.target.value)}
-              />
-              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                <ChevronUpDownIcon className="h-5 w-5" aria-hidden="true" />
-              </Combobox.Button>
-            </div>
-            <Transition
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-              afterLeave={() => setQuery('')}
-            >
-              <Combobox.Options className="absolute nowheel mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg">
-                {filteredList.length === 0 && query !== '' ? (
-                  <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                    Nothing found.
-                  </div>
-                ) : (
-                  filteredList.map((item) => (
-                    <Combobox.Option
-                      key={item}
-                      className={({ active }) =>
-                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                          active ? 'bg-teal-600 text-white' : 'text-gray-900'
-                        }`
-                      }
-                      value={item}
-                    >
-                      {({ selected, active }) => (
-                        <>
-                          <span
-                            className={`block truncate ${
-                              selected ? 'font-medium' : 'font-normal'
-                            }`}
-                          >
-                            {flagAndCountry(item)}
-                          </span>
-                          {selected ? (
+          {({ open }) => (
+            <div className="relative">
+              <div className="flex flex-row items-center w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow">
+                { !open && 
+                  <ReactCountryFlag svg countryCode={selectedItem} className="absolute left-3 border" />
+                }
+                <Combobox.Input
+                  className="nodrag w-full border-none py-2 pl-10 pr-10 text-sm leading-5 text-gray-900 rounded-lg"
+                  placeholder="Choose an option"
+                  displayValue={() => (countryInfo[selectedItem] ?? [])['name']}
+                  onChange={(event) => setQuery(event.target.value)}
+                />
+                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                  <ChevronUpDownIcon className="h-5 w-5" aria-hidden="true" />
+                </Combobox.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                afterLeave={() => setQuery('')}
+              >
+                <Combobox.Options className="absolute nowheel mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-sm shadow-lg">
+                  {filteredList.length === 0 && query !== '' ? (
+                    <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                      Nothing found.
+                    </div>
+                  ) : (
+                    filteredList.map((item) => (
+                      <Combobox.Option
+                        key={item}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                            active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                          }`
+                        }
+                        value={item}
+                      >
+                        {({ selected, active }) => (
+                          <>
                             <span
-                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                active ? 'text-white' : 'text-teal-600'
+                              className={`block truncate ${
+                                selected ? 'font-medium' : 'font-normal'
                               }`}
                             >
-                              <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              {flagAndCountry(item)}
                             </span>
-                          ) : null}
-                        </>
-                      )}
-                    </Combobox.Option>
-                  ))
-                )}
-              </Combobox.Options>
-            </Transition>
-          </div>
+                            {selected ? (
+                              <span
+                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                  active ? 'text-white' : 'text-teal-600'
+                                }`}
+                              >
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Combobox.Option>
+                    ))
+                  )}
+                </Combobox.Options>
+              </Transition>
+            </div>
+          )}
         </Combobox>
       </div>
     </div>
-  );
+  )
 }
