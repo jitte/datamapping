f20a49b1cc26710528bd7aad7234144981b2a3e1
jitte <jitte@jitte.net>
2023-07-17 18:49:29 +0900

    added preference menu

---
+++ src/components/menu/preference.tsx
@@ -0,0 +1,25 @@
+import {
+  MenubarContent,
+  MenubarMenu,
+  MenubarSub,
+  MenubarTrigger,
+} from '@/components/ui/menubar'
+import { Cog } from 'lucide-react'
+import { ProjectsDialog } from '@/projects'
+
+const PreferenceMenu = () => {
+  return (
+    <MenubarMenu>
+      <MenubarTrigger>
+        <Cog size={16} />
+      </MenubarTrigger>
+      <MenubarContent>
+        <MenubarSub>
+          <ProjectsDialog />
+        </MenubarSub>
+      </MenubarContent>
+    </MenubarMenu>
+  )
+}
+
+export { PreferenceMenu }
