806f42c6b70c1875becf3b34c050883c0ffbb7c8
jitte <jitte@jitte.net>
2023-07-17 21:27:28 +0900

    click to select node

--- src/components/menu/find.tsx
+++ src/components/menu/find.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import { Node } from 'reactflow'
 import {
   MenubarMenu,
@@ -10,32 +11,63 @@ import {
   MenubarCheckboxItem,
   MenubarSeparator,
 } from '@/components/ui/menubar'
+import { Badge } from '@/components/ui/badge'
+import { Share2 } from 'lucide-react'
+
 import { useLocalStore } from '@/lib/store'
+import { DataFlowContext } from '@/contexts/dataFlowContext'
 import { ProjectType } from '@/projects/types'
+import { CountryFlag } from '../nodes/config/country'
 
 const FindMenu = () => {
-  const { projects, preference, storePreference } = useLocalStore()
+  const {
+    projects,
+    storeProjects,
+    currentProjectId,
+    storeCurrentProjectId,
+    preference,
+    storePreference,
+  } = useLocalStore()
+  const { setNodes } = useContext(DataFlowContext)
 
   const handleShowOnlyNamedNode = () => {
-    console.log('at: FindMenu/handleShowOnlyNamedNode', preference)
     storePreference({
       ...preference,
       showOnlyNamedNode: !preference.showOnlyNamedNode,
     })
-    console.log('and so?', preference)
+  }
+
+  const handleClick = (project: ProjectType, node: Node) => {
+    //node.selected = true
+    project.nodes.map((nd: Node) => nd.selected = nd.id === node.id)
+    if (project.id !== currentProjectId) {
+      storeProjects([...projects])
+      storeCurrentProjectId(project.id)
+    } else {
+      setNodes([...project.nodes])
+    }
+  }
+
+  const NodeMenubarItem = ({ node }: { node: Node }) => {
+    return (
+      <div className='flex flex-row items-center gap-1'>
+        <CountryFlag countryCode={node.data.country} />
+        <Badge variant="outline">{node.data.role}</Badge>
+        {node.data.name}
+      </div>
+    )
   }
 
   const NodeMenubarItems = ({ project }: { project: ProjectType }) => {
     return (
       <MenubarSubContent>
         {project.nodes.map((node: Node) =>
-          preference.showOnlyNamedNode ? (
-            node.data.name ? (
-              <MenubarItem key={node.id}>{node.data.name}</MenubarItem>
-            ) : null
-          ) : (
-            <MenubarItem key={node.id}>
-              {node.data.name ?? 'No Name'}
+          preference.showOnlyNamedNode && !node.data.name ? null : (
+            <MenubarItem
+              key={`pj_${project.id}_${node.id}`}
+              onClick={() => handleClick(project, node)}
+            >
+              <NodeMenubarItem node={node} />
             </MenubarItem>
           )
         )}
@@ -44,10 +76,17 @@ const FindMenu = () => {
   }
 
   const ProjectMenubarSub = () => {
-    return projects.map((pj) => (
-      <MenubarSub key={pj.id}>
-        <MenubarSubTrigger inset>{pj.name}</MenubarSubTrigger>
-        <NodeMenubarItems project={pj} />
+    return projects.map((project) => (
+      <MenubarSub key={`pj_${project.id}`}>
+        {project.id === currentProjectId ? (
+          <MenubarSubTrigger>
+            <Share2 size={16} className='mr-2'/>
+            {project.name}
+          </MenubarSubTrigger>
+        ) : (
+          <MenubarSubTrigger inset>{project.name}</MenubarSubTrigger>
+        )}
+        <NodeMenubarItems project={project} />
       </MenubarSub>
     ))
   }
