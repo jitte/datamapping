016c7e865b578595a3b94d2ee49d548a90c58c25
jitte <jitte@jitte.net>
2023-07-29 18:21:25 +0900

    added debug info

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,6 +1,11 @@
 import { useContext } from 'react'
 import { Menubar } from '@/components/ui/menubar'
-import { Button } from '../ui/button'
+import { Button } from '@/components/ui/button'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 import { Flame } from 'lucide-react'
 
 import { FileMenu } from './file'
@@ -10,11 +15,13 @@ import { FindMenu } from './find'
 import { ProjectMenu } from './projects'
 import { PreferenceMenu } from './preference'
 
+import { useLocalStore } from '@/lib/store'
 import { DataFlowContext } from '@/contexts/dataFlowContext'
 import { AutoLayout } from '@/lib/layout'
 
 export function MyMenubar() {
-  const { setLayout, reactFlowInstance } = useContext(DataFlowContext)
+  const { layout, setLayout, reactFlowInstance } = useContext(DataFlowContext)
+  const { preference } = useLocalStore()
 
   const handleTemperature = () => {
     if (reactFlowInstance) {
@@ -23,6 +30,33 @@ export function MyMenubar() {
       setLayout(layout)
     }
   }
+  const DebugInfo = () => {
+    const viewport = reactFlowInstance?.getViewport()
+    const vpx = viewport?.x
+    const vpy = viewport?.y
+    const vpz = viewport?.zoom
+    const centerX = AutoLayout.center.x.toFixed(2)
+    const centerY = AutoLayout.center.y.toFixed(2)
+
+    return preference.showDebugInfo ? (
+      <HoverCardContent>
+        <div className="text-xs text-left">
+          <h2>Reactflow Instance</h2>
+          <ul className='ml-2 text-gray-500'>
+            <li>viewport: ({`${vpx}, ${vpy}, ${vpz}`})</li>
+            <li></li>
+          </ul>
+          <h2>Auto Layout</h2>
+          <ul className="ml-2 text-gray-500">
+            <li>center: ({`${centerX}, ${centerY}`})</li>
+            <li>layoutWidth: {layout?.layoutWidth.toFixed(2)}</li>
+            <li>layoutHeight: {layout?.layoutHeight.toFixed(2)}</li>
+          </ul>
+        </div>
+      </HoverCardContent>
+    ) : null
+  }
+
   return (
     <Menubar className="bg-white/50">
       <FileMenu />
@@ -37,8 +71,15 @@ export function MyMenubar() {
         onClick={handleTemperature}
         className="flex flex-row items-center gap-2"
       >
-        <Flame size={20} />
-        {AutoLayout.temperature.toFixed(2)}
+        <HoverCard>
+          <HoverCardTrigger>
+            <div className="flex flex-row items-center">
+              <Flame size={20} />
+              {AutoLayout.temperature.toFixed(2)}
+            </div>
+          </HoverCardTrigger>
+          <DebugInfo />
+        </HoverCard>
       </Button>
       <PreferenceMenu />
     </Menubar>
