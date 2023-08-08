bceb5d60c650c58de6185e66db1ae669748d24c8
jitte <jitte@jitte.net>
2023-08-08 13:51:12 +0900

    implemented import

--- src/components/menu/file.tsx
+++ src/components/menu/file.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { useReactFlow, getRectOfNodes } from 'reactflow'
 import {
   MenubarContent,
@@ -21,11 +21,16 @@ import {
   DialogFooter,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label'
 
 import { useLocalStore } from '@/lib/store'
 import { downloadImage } from '@/lib/image'
+import { DataFlowContext } from '@/lib/contexts'
+import { ProjectType } from '@/components/projects/types'
+import { newProjectId } from '@/components/projects/utils'
 
-function DownloadMenu() {
+const DownloadMenu = () => {
   const { getNodes } = useReactFlow()
 
   function onClick(format: 'svg' | 'png') {
@@ -47,12 +52,12 @@ function DownloadMenu() {
   )
 }
 
-function ExportProjects() {
+const ExportProjects = () => {
   const projects = useLocalStore((state) => state.projects)
   const [fileName, setFileName] = useState('Data mapping')
   const [open, setOpen] = useState(false)
 
-  function normalCaseToSnakeCase(str: string) {
+  const normalCaseToSnakeCase = (str: string) => {
     return str
       .split(' ')
       .map((word, index) => {
@@ -64,7 +69,7 @@ function ExportProjects() {
       .join('_')
   }
 
-  function downloadFlow() {
+  const downloadFlow = () => {
     // create a data URI with the current flow data
     const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
       JSON.stringify(projects, null, '\t')
@@ -81,52 +86,151 @@ function ExportProjects() {
   }
 
   return (
-    <MenubarSub>
-      <Dialog open={open} onOpenChange={() => setOpen(!open)}>
-        <DialogTrigger className="w-full px-2 py-1.5 text-left text-sm hover:bg-accent rounded-sm">
-          Export...
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Export projects</DialogTitle>
-            <DialogDescription>
-              Export all projects in JSON format.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid items-center grid-cols-4 gap-4">
-              <div>Filename</div>
-              <Input
-                id="name"
-                className="col-span-3"
-                value={fileName}
-                onChange={(event) => {
-                  setFileName(event.target.value)
-                }}
-              />
-            </div>
+    <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+      <DialogTrigger className="w-full px-2 py-1.5 text-left text-sm hover:bg-accent rounded-sm">
+        Export...
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Export Dialog</DialogTitle>
+          <DialogDescription>
+            Export all projects in JSON format.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid items-center grid-cols-4 gap-4">
+            <div>Filename</div>
+            <Input
+              id="name"
+              className="col-span-3"
+              value={fileName}
+              onChange={(event) => {
+                setFileName(event.target.value)
+              }}
+            />
           </div>
-          <DialogFooter>
-            <Button type="submit" onClick={downloadFlow}>
-              Export
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </MenubarSub>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={downloadFlow}>
+            Export
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
-export function FileMenu() {
+const ImportProjects = () => {
+  const { projects, storeProjects } = useLocalStore()
+  const { incrementNodeId } = useContext(DataFlowContext)
+  const [open, setOpen] = useState(false)
+  const [preserve, setPreserve] = useState(true)
+  const [preview, setPreview] = useState('')
+  let newProjects: ProjectType[] = []
+
+  const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target
+    if (!target.files) return
+    const file = target.files[0]
+    console.log(file)
+    // check if the file type is application/json
+    if (!file || file.type !== 'application/json') {
+      setPreview(
+        '**ERROR** File format is not JSON.'
+      )
+      return
+    }
+    file
+      .text()
+      .then((text) => {
+        setPreview(text)
+      })
+      .catch((reason) => {
+        setPreview(reason)
+      })
+  }
+
+  const handleImport = () => {
+    try {
+      newProjects = JSON.parse(preview) as ProjectType[]
+    } catch {
+      setPreview('***ERROR*** Could not parse JSON.')
+      return
+    }
+    if (preserve) {
+      let id = newProjectId(projects)
+      for (const pj of newProjects) {
+        pj.id = id++
+        const nodeMap: { [key: string]: string } = {}
+        pj.nodes.forEach((node) => {
+          const newId = `node_${incrementNodeId()}`
+          nodeMap[node.id] = newId
+          node.id = newId
+        })
+        pj.edges.forEach((edge) => {
+          edge.source = nodeMap[edge.source]
+          edge.target = nodeMap[edge.target]
+        })
+      }
+      storeProjects([...newProjects, ...projects])
+    } else {
+      storeProjects(newProjects)
+    }
+    setOpen(false)
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+      <DialogTrigger className="w-full px-2 py-1.5 text-left text-sm hover:bg-accent rounded-sm">
+        Import...
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Import Dialog</DialogTitle>
+          <DialogDescription>
+            Import projects from local JSON file.
+          </DialogDescription>
+        </DialogHeader>
+        <Label>Select File</Label>
+        <Input type="file" onChange={handleUpload} />
+        <Label>Preview Data </Label>
+        <div className="w-full h-20 p-4 overflow-scroll text-xs text-gray-500 border rounded-md">
+          {preview}
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <Checkbox
+            id="preserve"
+            checked={preserve}
+            onCheckedChange={(value) => setPreserve(!!value)}
+          />
+          <Label htmlFor="preserve">Preserve current projects</Label>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={handleImport}>
+            Import
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+const FileMenu = () => {
   return (
     <MenubarMenu>
       <MenubarTrigger>File</MenubarTrigger>
       <MenubarContent>
         <DownloadMenu />
         <MenubarSeparator />
-        <MenubarItem disabled>Import...</MenubarItem>
-        <ExportProjects />
+        <MenubarSub>
+          <ImportProjects />
+        </MenubarSub>
+        <MenubarSub>
+          <ExportProjects />
+        </MenubarSub>
       </MenubarContent>
     </MenubarMenu>
   )
 }
+
+export { FileMenu }
